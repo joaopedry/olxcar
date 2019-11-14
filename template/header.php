45405c12ab58cca1d65ed9a2bdd227415f85e701
joaopedry <36898182+joaopedry@users.noreply.github.com>
2019-11-14 20:53:19 -0300

    Add templates

---
+++ template/header.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" type="text/css" href="template/stlyle.css">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta charset="utf-8">
+<button class="areaRestrita">Área Restrita</button>
+<header>
+  OLXCar
+</header>
---
+++ template/lateral.php
@@ -0,0 +1,11 @@
+<aside>
+	<nav class="menu">
+		<ul>
+        	<a href="index.php"><li>Inicio</li></a>
+        	<a href="cadastraanuncio.php"><li>Cadastrar Anuncio</li></a>
+            <!-- <a href="buscaAnuncio.php"><li>Pesquisar Anuncio</li></a> -->
+            <a href="exibeMeuAnuncio.php"><li>Meus Anuncios</li></a>
+        	<a href="sair.php"><li>Sair</li></a>
+    	</ul>
+    </nav>
+</aside>
